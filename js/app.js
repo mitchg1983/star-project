c8eb9563b926b915bb42b11e7503dc130b4ddd1e
mitchg1983 <mitchg83@Geoffreys-MacBook-Pro.local>
2022-05-18 14:37:28 -0400

    Add working star visuals.

--- js/app.js
+++ js/app.js
@@ -1,29 +1,37 @@
-function removeStar() {
-  let starOutput = Number(document.getElementById("StarOutput").innerHTML);
-  console.log("removeStar...", starOutput);
-
-  if (starOutput > 1) {
-    document.getElementById("StarOutput").innerHTML = starOutput - 1;
-  }
-
-  return;
-}
-
-function addStar() {
-  console.log(starOutput + 1);
-
-  return;
-}
-
 function starClick(input) {
-  console.log("Starting starClick...");
   console.log("input is, ...", input);
   let starOutput = Number(document.getElementById("StarOutput").innerHTML);
+  let newRating = 0;
   console.log("current starOutput is, ...", starOutput);
 
   if (input === "add") {
-    if (starOutput < 5) {
-      document.getElementById("StarOutput").innerHTML = starOutput + 1;
+    if (starOutput === 5) {
+      return;
     }
+    newRating = starOutput + 1;
+    document.getElementById("StarOutput").innerHTML = newRating;
+  }
+
+  if (input === "remove") {
+    if (starOutput === 1) {
+      return;
+    }
+    newRating = starOutput - 1;
+    document.getElementById("StarOutput").innerHTML = newRating;
+  }
+
+  console.log("new starOutput rating is, ...", newRating);
+
+  const stars = document.getElementsByClassName("star");
+
+  for (star of stars) {
+    star.classList.remove("glow");
+    star.classList.add("glow-off");
+  }
+
+  for (let i = 0; i <= newRating - 1; i++) {
+    stars[i].classList.remove("glow-off");
+
+    stars[i].classList.add("glow");
   }
 }
